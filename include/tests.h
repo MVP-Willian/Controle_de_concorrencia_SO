ac31deedc77bb8569665d5eff3daa57360fd4be4
luiz.sena <luiz.sena@ufam.edu.br>
2025-10-02 11:20:28 -0400

    refactor(tests): ajustar formato de logs na macro `RUN_TEST`

--- include/tests.h
+++ include/tests.h
@@ -6,11 +6,11 @@
 #define TESTS_H
 
 #define RUN_TEST(test_name) do { \
-    printf("Runnig test: %s... ", #test_name); \
+    printf("Runnig test: %s\n", #test_name); \
     if(test_name()) { \
-        printf("PASSED\n"); \
+        printf("Result: PASSED\n"); \
     } else { \
-        printf("FAILED\n"); \
+        printf("Result: FAILED\n"); \
         test_failed++; \
     } \
     test_run++; \
