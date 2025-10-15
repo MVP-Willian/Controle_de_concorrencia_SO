7976b8d64a6e018cfa04a6632d5ce42b1c568aea
luiz.sena <luiz.sena@ufam.edu.br>
2025-10-15 15:40:30 -0400

    feat(reader-writer): adicionar funções writer sem controle e testes correspondentes

--- lib/reader-writer/no_control.c
+++ lib/reader-writer/no_control.c
@@ -14,24 +14,23 @@
 void* reader_no_control(void* arg)
 {
     const ThreadArgs* args = (ThreadArgs*)arg;
+    char titular[100];
+    char buffer[256];
 
     logger(READER, args->thread_id, "Thread Criada");
 
     sleep(1);
     logger(READER, args->thread_id, "Entrando na região crítica");
 
-    int numero_conta = args->conta->numero_conta;
-    double saldo = args->conta->saldo;
-
-    char titular[100];
+    const int numero_conta = args->conta->numero_conta;
+    const double saldo = args->conta->saldo;
     strncpy(titular, args->conta->titular, 99);
     titular[99] = '\0';
-
-    char buffer[256];
     snprintf(buffer, sizeof(buffer),
              "Numero da conta: %d | Saldo: %.2f | Titular: %s",
              numero_conta, saldo, titular);
     logger(READER, args->thread_id, buffer);
+
     logger(READER, args->thread_id, "Saindo da região crítica");
     logger(READER, args->thread_id, "Thread Finalizada");
 
@@ -40,5 +39,24 @@ void* reader_no_control(void* arg)
 
 void* writer_no_control(void* arg)
 {
+    const ThreadArgs* args = (ThreadArgs*)arg;
+    char buffer[100];
+
+    logger(WRITER, args->thread_id, "Thread Criada");
+
+    sleep(1);
+
+    logger(WRITER, args->thread_id, "Entrando na região crítica");
+
+    snprintf(buffer, sizeof(buffer), "Saldo antes de atualizar: %.2f", args->conta->saldo);
+    logger(WRITER, args->thread_id, buffer);
+    args->conta->saldo += args->valor_operacao;
+    snprintf(buffer, sizeof(buffer), "Saldo após de atualizar: %.2f", args->conta->saldo);
+    logger(WRITER, args->thread_id, buffer);
+
+
+    logger(WRITER, args->thread_id, "Saindo da região crítica");
+    logger(WRITER, args->thread_id, "Thread Finalizada");
+
     return NULL;
 }
--- tests/reader-writer/no_control.test.c
+++ tests/reader-writer/no_control.test.c
@@ -11,15 +11,25 @@
 #include "../../include/reader-writer/reader_writer.h"
 #include "../../include/theme.h"
 
-static ContaBancaria *g_shared_conta;
+static ContaBancaria* g_shared_conta;
 
 int test_no_control_reader()
 {
     g_shared_conta = inicializa_conta(42, 100.0, "Usuário Teste");
 
-    ThreadArgs args = { .thread_id = 1, .conta = g_shared_conta, .valor_operacao = 0.0 };
+    ThreadArgs args = {.thread_id = 1, .conta = g_shared_conta, .valor_operacao = 0.0};
 
     reader_no_control(&args);
+    return 1;
+}
+
+int test_no_control_writer()
+{
+    g_shared_conta = inicializa_conta(42, 100.0, "Usuário Teste");
+
+    ThreadArgs args = {.thread_id = 1, .conta = g_shared_conta, .valor_operacao = -10.0};
+
+    writer_no_control(&args);
     return 1;
 }
 
@@ -28,6 +38,8 @@ int main()
     printf("--- Iniciando testes para logger ---\n");
 
     RUN_TEST(test_no_control_reader);
+    RUN_TEST(test_no_control_writer);
+
 
     printf("--------------------------------------\n");
 }
